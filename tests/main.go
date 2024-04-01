0dc7a88b4a618b66b7b921bfae6f41355487141b
Felipe Souza <blackreapersk@protonmail.com>
2024-04-01 13:51:36 -0300

    Create main.go

---
+++ tests/main.go
@@ -0,0 +1,22 @@
+package main
+
+// required_tools = ["subfinder", "amass", "httprobe", "nuclei", "katana", "ffuf", "gau"]
+
+// NUCLEI LIB
+// import nuclei "github.com/projectdiscovery/nuclei/v3/lib"
+// REF: https://pkg.go.dev/github.com/projectdiscovery/nuclei/v3/lib#section-readme
+
+// OWASP/AMASS LIB
+
+// "github.com/OWASP/Amass/v3/config"
+// "github.com/OWASP/Amass/v3/datasrcs"
+// "github.com/OWASP/Amass/v3/enum"
+// "github.com/OWASP/Amass/v3/format"
+// "github.com/OWASP/Amass/v3/requests"
+// "github.com/OWASP/Amass/v3/systems"
+// "github.com/caffix/netmap"
+// "github.com/caffix/stringset"
+// "github.com/fatih/color"
+// REF: https://github.com/owasp-amass/amass/blob/v3.22.2/cmd/amass/enum.go
+
+// [....]
